6c1d05ec869b0c4a55ded97c56c6a722212f7608
allenvancil <allenvancil@hotmail.com>
2025-01-04 07:37:54 -0900

    removed copied files to prevent confusion

--- basics/Py_52/basics/L04_modules.py
+++
@@ -1,6 +0,0 @@
-from tabulate import tabulate
-from util.create_utils import create_devices
-
-if __name__ == "__main__":
-    devices = create_devices(5,2)
-    print("\n", tabulate(devices, headers="keys"))
