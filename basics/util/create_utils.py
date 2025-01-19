7da81daa5696ef12c29adb38a71d646bb5f75924
allenvancil <allenvancil@hotmail.com>
2025-01-19 10:25:19 -0900

    fixed create_utils

--- basics/util/create_utils.py
+++ basics/util/create_utils.py
@@ -1,62 +1,92 @@
-from random import choice
+from random import choice, randint
 import string
 from pprint import pprint
+import time
 
-from pkg_resources import non_empty_lines
+allocated_ip_addresses = set()
 
-def create_device(num_dev=1, num_subnet=1):
+# A function that creates a single random ip address
 
-    created_devices = list()
-    if num_dev > 254 or num_subnet >254:
-        print("error")
-        return created_devices
-    
-    for subnet_index in range(1,num_subnet+1):
+def get_rand_ip(subnet_index):
+    # create random device index
+    for _ in range(1, 254*4):
+        device_index = randint(1, 254)
+        ip = "10.0." + str(subnet_index) + "." + str(device_index)
 
-        for device_index in range(1, num_dev+1):
-            
-            device = dict()
+        if ip in allocated_ip_addresses:
+            continue
+        allocated_ip_addresses.add(ip)
+        return ip
+    for device_index in range(1, 254):
+        ip = "10.0." + str(subnet_index) + "." + str(device_index)
 
+        if ip in allocated_ip_addresses:
+            continue
 
-            device["name"] = (
-                choice(["r2", "r3", "r4", "r6", "r10"])
-                + choice(["L", "U"])
-                + choice(string.ascii_letters)
-            )
+        allocated_ip_addresses.add(ip)
+    return None
+pprint(get_rand_ip(10))
 
-            device["vendor"] = choice(["cisco", "juniper", "arista"])
-            if device["vendor"] == "cisco":
-                device["os"] = choice(["ios", "iosxe", "nexus"])
-                device["version"] = choice(["12.1(T).04", "14.07X", "8.12(S).010", "20.45"])
-            elif device["vendor"] == "juniper":
-                device["os"] = "junos"
-                device["version"] = choice(["J6.23.1", "8.43.12", "6.45", "6.03"])
-            elif device["vendor"] == "arista":
-                device["os"] = "eos"
-                device["version"] = choice(["2.45", "2.55", "2.92.145", "3.01"])
-            device["ip"] = "10.0." + str(subnet_index) + "." + str(device_index)
-            created_devices.append(device)
-    return created_devices
-# pprint(create_device(5,1))
+# Function creates a dictionary of a random device
 
-def create_devices(num_dev=1, num_subnet=1):
+def create_device(device_index, subnet_index, random_ip=False):
+
+    device = dict()
+
+    device["name"] = (
+        choice(["r2", "r3", "r4", "r6", "r10"])
+        + choice(["L", "U"])
+        + choice(string.ascii_letters)
+    )
+
+    # RANDOM VENDOR FROM CHOICE OF CISCO, JUNIPER, ARISTA
+    device["vendor"] = choice(["cisco", "juniper", "arista"])
+    if device["vendor"] == "cisco":
+        device["os"] = choice(["ios", "iosxe", "iosxr", "nexus"])
+        if device["os"] == "ios":
+            device["version"] = choice(["15", "15E", "15SY", "12.2SE"])
+        elif device["os"] == "iosxe":
+            device["version"] = choice(["16.9", "16.7", "16.5", "16.3"])
+        elif device["os"] == "iosxr":
+            device["version"] = choice(["6.2", "6.0", "5.4", "5.1"])
+        elif device["os"] == "nexus":
+            device["version"] = choice(["8.2", "8.0", "7.3", "7.1"])
+    elif device["vendor"] == "juniper":
+        device["os"] = "junos"
+        device["version"] = choice(["12.3R12-S15", "15.1R7-S6", "18.4R2-S3", "15.1X53-D591"])
+    elif device["vendor"] == "arista":
+        device["os"] = "eos"
+        device["version"] = choice(["4.24.1F", "4.23.2F", "4.22.1F", "4.21.3F"])
+
+    if random_ip:
+        device["ip"] = get_rand_ip(subnet_index)
+    else:
+        device["ip"] = "10.0." + str(subnet_index) + "." + str(device_index)
+
+    return device
+# pprint(create_device(5, 10))
+
+def create_devices(num_dev=1, num_subnet=1, random_ip=False):
+
     created_devices = list()
 
     if num_dev > 254 or num_subnet > 254:
         print("error: too many subnet or devices")
         return created_devices
 
-    print("Beginning device creation")
+    print("Beginning device creation...")
+    time.sleep(2)
     for subnet_index in range(1, num_subnet+1):
 
         for device_index in range(1, num_dev+1):
 
-            device = create_device(device_index, subnet_index)
+            device = create_device(device_index, subnet_index, random_ip)
             created_devices.append(device)
 
-    print("completed device creation")
+
     return created_devices
 # pprint(create_devices(5,1))
+print("completed device creation")
 
 def create_devices_tuple(num_dev=1, num_subnet=1):
 
@@ -66,11 +96,11 @@ pprint(create_devices_tuple(5,1))
 
 def create_network(num_dev=1, num_subnet=1):
 
-    devices = create_device(num_dev, num_subnet)
+    devices = create_devices(num_dev, num_subnet)
     network = dict()
     network["subnets"] = dict()
     for device in devices:
-        subnet_address_bytes = device['ip'].split(".")
+        subnet_address_bytes = device["ip"].split(".")
         subnet_address_bytes[3] = "0"
         subnet_address = ".".join(subnet_address_bytes)
 
@@ -88,6 +118,15 @@ def create_network(num_dev=1, num_subnet=1):
             interfaces.append(interface)
         device["interfaces"] = interfaces
     return network
-#  pprint(create_network(5,2))
+pprint(create_network(5,2))
 
+def create_devices_gen(num_dev=1, num_subnet=1):
+    if num_dev > 254 or num_subnet > 254:
+        print("error: too many subnets or devices")
+        return None
+
+    for subnet_index in range(1, num_subnet+1):
+        for device_index in range(1, num_dev+1):
+            device = create_device(device_index, subnet_index)
+            yield device
 
