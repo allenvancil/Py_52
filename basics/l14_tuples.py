1cda994070d0346d8a3b0db64e40fb45d5d35dc4
allenvancil <allenvancil@hotmail.com>
2025-01-20 10:06:12 -0900

    finished tuple, works

--- basics/l14_tuples.py
+++ basics/l14_tuples.py
@@ -22,3 +22,35 @@ if __name__ == "__main__":
     print(" model", device.model)
     print("    os:", device.os)
     print("    ip:", device.ip)
+
+    print("\n---------------tuple with a for loop--------------\n")
+    item = ("name", "vendor", "model", "os", "ip")
+    for i in range(len(device)):
+        print(f"{item[i]:>10s} : {device[i]}")
+
+    print("\n---------------tuple with a for loop using enumerate--------------\n")
+    item = ("name", "vendor", "model", "os", "ip")
+    for i, device_item in enumerate(device):
+        print(f"{item[i]:>10s} : {device[i]}")
+
+    print("\n---------------pprint device tuple--------------\n")
+    pprint(device)
+
+    print("\n---------------convert devices into tuples--------------\n")
+    devices = create_devices(10,2, True)
+    devices_as_namedtuples = list()
+    for device in devices:
+        Device = namedtuple("Device", ["name", "vendor", "os", "version", "ip"])
+        devices_as_namedtuples.append(Device(**device))
+
+    print("\n---------------pprint devices named as tuples--------------\n")
+    pprint(devices_as_namedtuples)
+
+    print("\n--------pprint devices named as tuples better format--------------\n")
+    print("   Name      Vendor   :  OS     IP Address")
+    print("  ------    -------    -----    -----------")
+    for device in devices_as_namedtuples:
+        print(f"{device.name:>7}   {device.vendor:>10}  : {device.os:<6}  {device.ip:<15}")
+
+
+
