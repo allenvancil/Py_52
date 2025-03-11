8e4d9ddca1bb18132f847f440dc6d6a6d747cfd6
allenvancil <allenvancil@hotmail.com>
2025-03-10 20:43:17 -0700

    pyshark for many pkt

--- networking/pyshark_ex.py
+++ networking/pyshark_ex.py
@@ -8,6 +8,9 @@ def print_packet_summary(pkt):
 # CAPTURE EVERYTHING AND PRINT PACKET SUMMARIES
 print("\n----- Packet summaries --------------------")
 capture = pyshark.LiveCapture(interface='enp0s3', only_summaries=True)
-capture.sniff(packet_count=10)
-for packet in capture:
+
+print("\n----- Packet summaries --------------------")
+for i, packet in enumerate(capture.sniff_continuously()):
     print_packet_summary(packet)
+    if i >= 9:  # Stop after 10 packets
+        break
